dffd15e5a01ee655b342fcd1bacfa580316b5d03
Camilo Bravo <hola@cambraca.com>
2018-05-28 10:41:18 -0500

    Icon, splash screen, orientation locked to portrait

--- App.js
+++ App.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View, WebView } from 'react-native';
+import {StyleSheet, Text, View, WebView} from 'react-native';
+import {AppLoading} from 'expo';
 
 export default class App extends React.Component {
+  state = {
+    isReady: false,
+  };
+
+  async load() {
+    function timeout(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    console.log('A');
+    await timeout(3000);
+    console.log('B');
+  }
+
   render() {
+    if (!this.state.isReady)
+      return (
+        <AppLoading startAsync={this.load}
+                    onFinish={() => this.setState({isReady: true})}
+                    onError={console.error}
+        />
+      );
+
     return (
       <View style={styles.container}>
         <Text>Mini-test with kobo toolbox form</Text>
