a5eb083c103238f939b0953051e9321699e8fbe0
Camilo Bravo <hola@cambraca.com>
2018-05-30 20:46:22 -0500

    Test debug

--- src/components/Test.js
+++ src/components/Test.js
@@ -5,10 +5,10 @@ import {Button, Text, View} from 'react-native';
 import persist from "../persist";
 
 const Test = ({isOnline, doIt}) => (
-  <View>
+  <View style={{flexDirection: 'row', justifyContent: "space-evenly", width: "100%", alignItems: "center"}}>
     <Text>{isOnline ? 'online' : 'offline'}</Text>
     <Button onPress={() => doIt(!isOnline)} title="toggle"/>
-    <Button onPress={() => persist.clearAll()} title="clear all asyncstorage"/>
+    <Button onPress={() => persist.clearAll()} title="clear all"/>
   </View>
 );
 
