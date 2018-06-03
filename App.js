b41fed03e017823fde3eb03fe5a87286900d2f40
Camilo Bravo <hola@cambraca.com>
2018-06-02 19:18:42 -0500

    A quick way to clear everything when debugging (just uncomment these 2 lines)

--- App.js
+++ App.js
@@ -5,8 +5,13 @@ import {Provider} from 'react-redux';
 import AppContainer from './src/containers/AppContainer';
 import persist from "./src/persist";
 import store from "./src/store";
+import {logout} from "./src/actions";
 
 persist.store = store;
+
+// TEMP: clear everything (logout, etc)
+// persist.clearAll();
+// store.dispatch(logout());
 
 export default () => (
   <Provider store={store}>
