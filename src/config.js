20045c1542a36adda87307ff0194bc7481478cf8
Camilo Bravo <hola@cambraca.com>
2018-06-06 13:23:19 -0500

    Dev site without http auth

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ const config = {
   // no trailing slash on baseUrl
   // baseUrl: 'https://www.sheltercluster.org', //prod
   baseUrl: 'http://dev.sheltercluster.org', //dev
-  axiosExtra: {auth: {username: "shelter", password: "cluster"}},
+  axiosExtra: {},
+  // axiosExtra: {auth: {username: "shelter", password: "cluster"}},
   // baseUrl: 'http://local.sheltercluster.org.192.168.0.11.xip.io', //local
 };
 
