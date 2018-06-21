48b5aefdd4a09884fcf16a08a54ce3217910402d
Camilo Bravo <hola@cambraca.com>
2018-06-21 15:04:48 -0500

    Hourglasses font (not working yet)

--- src/components/NavTitle.js
+++ src/components/NavTitle.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import {Platform, StyleSheet, Text} from 'react-native';
 import {FontAwesome} from '@expo/vector-icons';
+import {Hourglasses} from '../hourglasses';
 import vars from "../vars";
 
 const NavTitle = ({online, title, downloading}: {
@@ -16,16 +17,41 @@ const NavTitle = ({online, title, downloading}: {
     icon = <FontAwesome key="online" name="wifi" size={20} color={vars.ACCENT_RED}/>;
   else if (downloading.total > 0) {
     const downloadingIcons = [
-      'hourglass-start',
-      'hourglass-half',
-      'hourglass-end',
+      // 'hourglass-start',
+      // 'hourglass-half',
+      // 'hourglass-end',
+      'hourglass-01',
+      'hourglass-02',
+      'hourglass-03',
+      'hourglass-04',
+      'hourglass-05',
+      'hourglass-06',
+      'hourglass-07',
+      'hourglass-08',
+      'hourglass-09',
+      'hourglass-10',
+      'hourglass-11',
+      'hourglass-12',
+      'hourglass-13',
+      'hourglass-14',
+      'hourglass-15',
+      'hourglass-16',
+      'hourglass-17',
+      'hourglass-18',
+      'hourglass-19',
+      'hourglass-20',
+      'hourglass-21',
+      'hourglass-22',
+      'hourglass-23',
+      'hourglass-24',
     ];
 
     const downloadingIcon = downloadingIcons[Math.min(
       downloadingIcons.length - 1,
       Math.floor(downloading.value / downloading.total * downloadingIcons.length)
     )];
-    icon = <FontAwesome key="downloading" name={downloadingIcon} size={20}/>;
+    // icon = <FontAwesome key="downloading" name={downloadingIcon} size={20}/>;
+    icon = <Hourglasses key="downloading" name={downloadingIcon} size={20}/>;
   }
 
   if (icon === null)
---
+++ src/hourglasses.js
@@ -0,0 +1,30 @@
+import createIconSet from '@expo/vector-icons/createIconSet';
+
+const glyphMap = {
+  "hourglass-01": 65, //'A'
+  "hourglass-02": 66, //'B'
+  "hourglass-03": 67, //'C'
+  "hourglass-04": 68, //'D'
+  "hourglass-05": 69, //'E'
+  "hourglass-06": 70, //'F'
+  "hourglass-07": 71, //'G'
+  "hourglass-08": 72, //'H'
+  "hourglass-09": 73, //'I'
+  "hourglass-10": 74, //'J'
+  "hourglass-11": 75, //'K'
+  "hourglass-12": 76, //'L'
+  "hourglass-13": 77, //'M'
+  "hourglass-14": 78, //'N'
+  "hourglass-15": 79, //'O'
+  "hourglass-16": 80, //'P'
+  "hourglass-17": 81, //'Q'
+  "hourglass-18": 82, //'R'
+  "hourglass-19": 83, //'S'
+  "hourglass-20": 84, //'T'
+  "hourglass-21": 85, //'U'
+  "hourglass-22": 86, //'V'
+  "hourglass-23": 87, //'W'
+  "hourglass-24": 88, //'X'
+};
+
+export default createIconSet(glyphMap, 'hourglasses', require('../assets/hourglasses.ttf'));
