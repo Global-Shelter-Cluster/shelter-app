4427279843adb2864769ad79111a04619bba6996
Camilo Bravo <hola@cambraca.com>
2018-07-02 13:04:41 -0500

    Styled login form

--- src/screens/auth/Login.js
+++ src/screens/auth/Login.js
@@ -1,58 +1,49 @@
 // @flow
 
 import React from 'react';
-import {ImageBackground, StyleSheet, Text, View, StatusBar, TextInput} from 'react-native';
+import {ImageBackground, StatusBar, StyleSheet, Text, TextInput, View, KeyboardAvoidingView} from 'react-native';
 import Button from "../../components/Button";
 import vars from "../../vars";
+import t from 'tcomb-form-native';
 
-let creds = {
-  'password': '',
-  'username': '',
-}
+const Form = t.form.Form;
 
-export default ({login, online, loggingIn}: {
+type Props = {
   login: () => {},
   online: boolean,
   loggingIn: boolean,
-}) => {
-  let button;
-  let inputs;
+}
 
-  if (!online)
-    button = <Text style={styles.text}>No internet connection detected.</Text>;
-  else if (loggingIn)
-    button = <Text style={styles.text}>Logging in...</Text>;
-  else {
-    button = <Button primary onPress={() => login(creds.username, creds.password)} title="Log in"/>;
-    inputs = (
-      <View>
-        <Text>Username:</Text>
-        <TextInput
-          style={styles.textinput}
-          onChangeText={(text) => creds.username = text}
-        />
-        <Text>Password:</Text>
-        <TextInput
-          style={styles.textinput}
-          onChangeText={(text) => creds.password = text}
-        />
+export default class Login extends React.Component<Props> {
+  render() {
+    const {login, online, loggingIn} = this.props;
+
+    let button;
+    if (!online)
+      button = <Text style={styles.text}>No internet connection detected.</Text>;
+    else if (loggingIn)
+      button = <Text style={styles.text}>Logging in...</Text>;
+    else {
+      button = <Button primary onPress={() => {
+        const value = this.refs.form.getValue();
+        if (value)
+          login(value.username, value.password)
+      }} title="Log in"/>;
+    }
+
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <StatusBar barStyle="light-content"/>
+        <ImageBackground style={styles.image} source={require('../../../assets/login.jpg')}>
+        </ImageBackground>
+        <View style={styles.innerContainer}>
+          <Text style={styles.title}>{"Shelter Cluster\nApp Prototype"}</Text>
+          {online && !loggingIn && <Form ref="form" type={formFields} options={formOptions}/>}
+          {button}
         </View>
+      </KeyboardAvoidingView>
     );
   }
-
-  return (
-    <View style={styles.container}>
-      <StatusBar
-        barStyle="light-content"
-      />
-      <ImageBackground style={styles.image} source={require('../../../assets/login.jpg')}/>
-      <View style={styles.innerContainer}>
-        <Text style={styles.title}>{"Shelter Cluster\nApp Prototype"}</Text>
-        {inputs}
-        {button}
-      </View>
-    </View>
-  );
 }
 
 const styles = StyleSheet.create({
@@ -61,14 +52,15 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   image: {
-    width: "100%",
-    height: "55%",
-    flexShrink: 1,
+    flex: 1,
+    padding: 20,
+    justifyContent: "flex-end",
   },
   innerContainer: {
+    // flexShrink: 1,
     padding: 20,
     paddingBottom: 30,
-    height: 165,
+    // height: 165,
     justifyContent: "space-between",
   },
   title: {
@@ -76,13 +68,46 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontWeight: "bold",
     color: vars.SHELTER_RED,
+    marginBottom: 20,
   },
   text: {
     fontSize: 18,
     textAlign: "center",
-    color: "white",
+    color: "black",
+    marginBottom: 0,
   },
-  textinput: {
-    height: 40,
-  }
 });
+
+const formStyles = {
+  ...Form.stylesheet,
+  textbox: {
+    ...Form.stylesheet.textbox,
+    normal: {
+      ...Form.stylesheet.textbox.normal,
+      borderColor: vars.SHELTER_RED,
+      borderRadius: 2,
+    },
+    error: {
+      ...Form.stylesheet.textbox.error,
+      borderColor: vars.ACCENT_RED,
+      borderRadius: 2,
+    },
+  },
+};
+
+const formFields = t.struct({
+  username: t.String,
+  password: t.String,
+});
+
+const formOptions = {
+  label: null,
+  auto: "placeholders",
+  stylesheet: formStyles,
+  fields: {
+    password: {
+      password: true,
+      secureTextEntry: true,
+    },
+  },
+};
