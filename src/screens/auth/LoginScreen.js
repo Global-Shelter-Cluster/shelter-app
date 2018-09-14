a1358250fb113b389fe7cf7518237c76f0722a5c
Camilo Bravo <hola@cambraca.com>
2018-09-14 10:09:00 +0200

    Solving conflict with screen names in nav.js

--- src/screens/auth/LoginScreen.js
+++ src/screens/auth/LoginScreen.js
@@ -29,12 +29,12 @@ class LoginScreen extends React.Component<Props> {
 
   componentDidMount() {
     if (this.props.loggedIn)
-      this.props.navigation.navigate('User');
+      this.props.navigation.navigate('Me');
   }
 
   componentDidUpdate() {
     if (this.props.loggedIn)
-      this.props.navigation.navigate('User');
+      this.props.navigation.navigate('Me');
   }
 
   render() {
